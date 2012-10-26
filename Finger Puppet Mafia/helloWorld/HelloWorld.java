fa1ca0e6a1ff63cc72484cd79d222b8b3d26ed0a
Anthony <afisher@wpi.edu>
2012-10-26 15:13:18 -0400

    Added an applet

---
+++ Finger Puppet Mafia/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+import java.applet.Applet;
+import java.awt.HeadlessException;
+
+public class HelloWorld extends Applet {
+
+	public HelloWorld() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+}
