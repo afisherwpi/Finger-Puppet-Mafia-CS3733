df392d9ca3d2de97d457153be0c78d3ad2968c66
Stephen-Peters <Stephen_Peters@wpi.edu>
2012-11-07 15:57:24 -0500

    I did a few things this commit, most importantly I added functionality to the add button to switch the right hand panel of the application for an add form that the user will use to add a new example to the PEN, currently the save button doesn't do anything but return the user to the original screen, also the form could use some formatting work.  Finally I also changed the name of some of the fields in the class to follow a convention, previously about have of the entries started with capital letters.

--- Finger Puppet Mafia/Database/MainWindowApp.java
+++ Finger Puppet Mafia/Database/MainWindowApp.java
@@ -5,10 +5,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JSplitPane;
@@ -23,22 +29,51 @@ import javax.swing.tree.DefaultTreeModel;
 public class MainWindowApp extends JFrame {
 
 	private JPanel contentPane;
-	private String[] ComboList = new String[] {"List of Items"};
-	JPanel LeftPane;
-	JPanel RightPane;
+	private String[] comboList = new String[] {"List of Items"};
+	private JPanel leftPane;
+	private JPanel rightPane;
+	private JSplitPane splitPane;
 	private JTree tree;
-	private JButton AddButton;
-	private JTextField SearchBar;
+	private JButton addButton;
+	private JTextField searchBar;
 	private JScrollBar scrollBar;
-	private JPanel HeaderBar;
-	private JPanel BottomBar;
+	private JPanel headerBar;
+	private JPanel bottomBar;
 	private JTextArea textPane;
 	private JTextArea txtrPrinthellosnmichael;
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
-	private JPanel TagsPanel;
+	private JPanel tagsPanel;
 	private JLabel lblNewLabel_2;
 	private JTextField txtCPrintString;
+	
+	private JPanel addPane;
+	private JPanel addInstructionPane;
+	private JPanel addTitlePane;
+	private JPanel addKeyWordsPane;
+	private JPanel addProgrammingLanguagePane;
+	private JPanel addCodePane;
+	private JPanel addDependenciesPane;
+	private JPanel addPublicPane;
+	private JPanel addButtonPane;
+	
+	private JLabel lblAddInstruction;
+	private JLabel lblAddTitle;
+	private JLabel lblAddKeyWords;
+	private String[] possibleProgrammingLanguages = {"Java", "C", "C++/C", "Python", "Ruby", "Pascal", "Other..."};
+	private JLabel lblAddProgrammingLanguage;
+	private JLabel lblAddCode;
+	private JLabel lblAddDependencies;
+	private JLabel lblAddPublic;
+	
+	private JTextField tfAddTitle;
+	private JTextField tfAddKeyWords;
+	private JComboBox<String> comboAddProgrammingLanguage;
+	private JTextArea taAddCode;
+	private JTextField tfAddDependencies;
+	private JCheckBox cbAddPublic;
+	private JButton btnAddCancel;
+	private JButton btnAddSave;
 	/**
 	 * Launch the application.
 	 */
@@ -70,54 +105,54 @@ public class MainWindowApp extends JFrame {
 		
 		
 		/////
-		RightPane = new JPanel();
-		LeftPane = new JPanel();
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,LeftPane,RightPane);
+		rightPane = new JPanel();
+		leftPane = new JPanel();
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,leftPane,rightPane);
 		splitPane.setDividerLocation(200);
 		getContentPane().add(splitPane, BorderLayout.CENTER);
 		contentPane.add(splitPane, BorderLayout.CENTER);
-		RightPane.setLayout(new BorderLayout(0, 0));
+		rightPane.setLayout(new BorderLayout(0, 0));
 		
 		scrollBar = new JScrollBar();
-		RightPane.add(scrollBar, BorderLayout.EAST);
+		rightPane.add(scrollBar, BorderLayout.EAST);
 		
-		HeaderBar = new JPanel();
-		RightPane.add(HeaderBar, BorderLayout.NORTH);
-		HeaderBar.setLayout(new BorderLayout(0, 0));
+		headerBar = new JPanel();
+		rightPane.add(headerBar, BorderLayout.NORTH);
+		headerBar.setLayout(new BorderLayout(0, 0));
 		
 		lblNewLabel = new JLabel("Print Statement (with string)");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
-		HeaderBar.add(lblNewLabel, BorderLayout.CENTER);
+		headerBar.add(lblNewLabel, BorderLayout.CENTER);
 		
 		lblNewLabel_1 = new JLabel("<html>When you want to add a string to a print statement you use '%s' and then add your string after the comma in the print statement.</html>");
 		
-		HeaderBar.add(lblNewLabel_1, BorderLayout.SOUTH);
+		headerBar.add(lblNewLabel_1, BorderLayout.SOUTH);
 		
-		BottomBar = new JPanel();
-		RightPane.add(BottomBar, BorderLayout.SOUTH);
+		bottomBar = new JPanel();
+		rightPane.add(bottomBar, BorderLayout.SOUTH);
 		
-		TagsPanel = new JPanel();
-		BottomBar.add(TagsPanel);
+		tagsPanel = new JPanel();
+		bottomBar.add(tagsPanel);
 		
 		lblNewLabel_2 = new JLabel("Tags: ");
-		TagsPanel.add(lblNewLabel_2);
+		tagsPanel.add(lblNewLabel_2);
 		
 		txtCPrintString = new JTextField();
 		txtCPrintString.setEditable(false);
 		txtCPrintString.setText("C, Print, String");
-		TagsPanel.add(txtCPrintString);
+		tagsPanel.add(txtCPrintString);
 		txtCPrintString.setColumns(10);
 		
 		textPane = new JTextArea();
 		textPane.setEditable(false);
 		textPane.setText("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50");
 		textPane.setBackground(Color.LIGHT_GRAY);
-		RightPane.add(textPane, BorderLayout.WEST);
+		rightPane.add(textPane, BorderLayout.WEST);
 		
 		txtrPrinthellosnmichael = new JTextArea();
 		txtrPrinthellosnmichael.setText("Print('Hello %s\\n',\"Michael\");");
-		RightPane.add(txtrPrinthellosnmichael, BorderLayout.CENTER);
-		LeftPane.setLayout(new BorderLayout(0, 0));
+		rightPane.add(txtrPrinthellosnmichael, BorderLayout.CENTER);
+		leftPane.setLayout(new BorderLayout(0, 0));
 		
 		tree = new JTree();
 		tree.setModel(new DefaultTreeModel(
@@ -137,22 +172,102 @@ public class MainWindowApp extends JFrame {
 			}
 		));
 		tree.setEditable(true);
-		LeftPane.add(tree, BorderLayout.CENTER);
+		leftPane.add(tree, BorderLayout.CENTER);
 		
-		AddButton = new JButton("Add?");
-		LeftPane.add(AddButton, BorderLayout.SOUTH);
+		addButton = new JButton("Add?");
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				splitPane.setRightComponent(addPane);
+				addButton.setEnabled(false);
+			}
+		});
+		leftPane.add(addButton, BorderLayout.SOUTH);
 		
-		SearchBar = new JTextField();
-		SearchBar.setText("Search...");
-		LeftPane.add(SearchBar, BorderLayout.NORTH);
-		SearchBar.setColumns(10);
+		searchBar = new JTextField();
+		searchBar.setText("Search...");
+		leftPane.add(searchBar, BorderLayout.NORTH);
+		searchBar.setColumns(10);
 		splitPane.setContinuousLayout(true);
 		
-
+		addPane = new JPanel();
+		addInstructionPane = new JPanel();
+		addTitlePane = new JPanel();
+		addKeyWordsPane = new JPanel();
+		addProgrammingLanguagePane = new JPanel();
+		addCodePane = new JPanel();
+		addDependenciesPane = new JPanel();
+		addPublicPane = new JPanel();
+		addButtonPane = new JPanel();
 		
+		addPane.setLayout(new GridLayout(0, 1));
+		addInstructionPane.setLayout(new GridLayout(0, 1));
+		addTitlePane.setLayout(new GridLayout(0, 2));
+		addKeyWordsPane.setLayout(new GridLayout(0, 2));
+		addProgrammingLanguagePane.setLayout(new GridLayout(0, 2));
+		addCodePane.setLayout(new GridLayout(0, 2));
+		addDependenciesPane.setLayout(new GridLayout(0, 2));
+		addPublicPane.setLayout(new GridLayout(0, 2));
+		addButtonPane.setLayout(new GridLayout(0, 2));
 		
-
+		addPane.add(addInstructionPane);
+		addPane.add(addTitlePane);
+		addPane.add(addKeyWordsPane);
+		addPane.add(addProgrammingLanguagePane);
+		addPane.add(addCodePane);
+		addPane.add(addDependenciesPane);
+		addPane.add(addPublicPane);
+		addPane.add(addButtonPane);
+		
+		lblAddInstruction = new JLabel("Fill out the following feilds and click save inorder to add an entry to your Programmers Examples Notebook.");
+		lblAddTitle = new JLabel("Title:");
+		lblAddKeyWords = new JLabel("Key Word:");
+		lblAddProgrammingLanguage = new JLabel("Language:");
+		lblAddCode = new JLabel("Code:");
+		lblAddDependencies = new JLabel("Dependencies:");
+		lblAddPublic = new JLabel("Make Public?");
+		
+		tfAddTitle = new JTextField();
+		tfAddKeyWords = new JTextField();
+		comboAddProgrammingLanguage = new JComboBox<String>(possibleProgrammingLanguages);
+		taAddCode = new JTextArea();
+		tfAddDependencies = new JTextField();
+		cbAddPublic = new JCheckBox();
+		btnAddCancel = new JButton("Cancel");
+		btnAddSave = new JButton("Save");
+		
+		addInstructionPane.add(lblAddInstruction);
+		addTitlePane.add(lblAddTitle);
+		addKeyWordsPane.add(lblAddKeyWords);
+		addProgrammingLanguagePane.add(lblAddProgrammingLanguage);
+		addCodePane.add(lblAddCode);
+		addDependenciesPane.add(lblAddDependencies);
+		addPublicPane.add(lblAddPublic);
+		
+		addTitlePane.add(tfAddTitle);
+		addKeyWordsPane.add(tfAddKeyWords);
+		addProgrammingLanguagePane.add(comboAddProgrammingLanguage);
+		addCodePane.add(taAddCode);
+		addDependenciesPane.add(tfAddDependencies);
+		addPublicPane.add(cbAddPublic);
+		addButtonPane.add(btnAddCancel);
+		addButtonPane.add(btnAddSave);
+		
+		btnAddCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				splitPane.setRightComponent(rightPane);
+				addButton.setEnabled(true);
+			}
+		});
+		
+		btnAddSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				splitPane.setRightComponent(rightPane);
+				addButton.setEnabled(true);
+			}
+		});
 	}
 
 }
-
