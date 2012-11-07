281afae4076ef1e1e34a992c489dbb97cb83cdab
Trevor Behlman <tsbehlman@wpi.edu>
2012-11-06 23:56:36 -0500

    Implemented method for retrieving a list of categories - to be used in Controller.

--- Finger Puppet Mafia/Database/Database.java
+++ Finger Puppet Mafia/Database/Database.java
@@ -5,6 +5,8 @@ import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 import com.db4o.query.Query;
 import java.io.File;
+import java.util.List;
+import com.db4o.query.Predicate;
 
 public class Database {
 	private String dbName;
@@ -36,6 +38,20 @@ public class Database {
 		query.constrain(CodeExample.class); // tell the query we're looking for a code example
 		query.descend(field).constrain(value); // tell the query to search the code example's for a (field) equivalent to (value) (example: ...for a Name equivalent to John)
 		return query.execute(); // get the results of the query
+	}
+	
+	/**
+	 * @param ID the unique ID of the user whose CodeExamples we're looking for
+	 * @return a List of CodeExamples created by the user with the specified unique ID
+	 * 
+	 * @author Trevor Behlman
+	 */
+	public List<Category> getCategories(){
+		return db.query(new Predicate<Category>(){
+			public boolean match(Category category){
+				return true;
+			}
+		});
 	}
 	
 	public void deleteDatabase(){
